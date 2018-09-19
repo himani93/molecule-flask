80bafbab6d67dc9f7447427889ebda8195d5ac6b
iqbal.farabi <iqbal.farabi@go-jek.com>
2018-09-19 08:45:19 +0530

    [Himani|Iqbal] Added config for workers instance

--- tf-nodes/instance.tf
+++ tf-nodes/instance.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "control-plane" {
   count = 3
   name = "controller-${count.index-1}"
   machine_type = "n1-standard-1"
@@ -20,6 +20,35 @@ resource "google_compute_instance" "default" {
     network = "${google_compute_network.kubernetes-the-hard-way.self_link}"
     subnetwork = "${google_compute_subnetwork.kubernetes.name}"
     address = "10.240.0.1${count.index-1}"
+  }
+
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
+}
+
+resource "google_compute_instance" "workers" {
+  count = 3
+  name = "worker-${count.index-1}"
+  machine_type = "n1-standard-1"
+  zone = "${data.google-compute-regions.available}"
+
+  tags = ["kubernetes-the-hard-way", "controller"]
+
+  boot_disk {
+    initialize_params {
+      image {
+        family = "ubuntu-1804-lts"
+        project = "ubuntu-os-cloud"
+      }
+      size = 200
+    }
+  }
+
+  network_interface {
+    network = "${google_compute_network.kubernetes-the-hard-way.self_link}"
+    subnetwork = "${google_compute_subnetwork.kubernetes.name}"
+    address = "10.240.0.2${count.index-1}"
   }
 
   service_account {
